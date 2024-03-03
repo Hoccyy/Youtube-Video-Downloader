15bcf843697aea749038039786253dad87baf14e
Hoccyy <94509399+Hoccyy@users.noreply.github.com>
2024-03-03 16:07:55 -0500

    Updates

--- install.bat
+++ install.bat
@@ -1,2 +1,6 @@
-py -m venv env
-source env/bin/activate
+@echo off
+python -m venv env
+call env\Scripts\activate
+pip install bs4
+pip install pytube
+pip install requests
--- install.sh
+++ install.sh
@@ -1,2 +1,6 @@
+#!/bin/bash
 python3 -m venv env
 source env/bin/activate
+pip install bs4
+pip install pytube
+pip install requests
